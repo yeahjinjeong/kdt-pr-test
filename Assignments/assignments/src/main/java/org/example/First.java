8a18a666f3f07260bd38e7bf705e0253885874e0
jyna120 <jyna120@naver.com>
2024-03-10 10:22:42 +0900

    Update: First 문제 추가

--- Assignments/assignments/src/main/java/org/example/First.java
+++ Assignments/assignments/src/main/java/org/example/First.java
@@ -16,6 +16,17 @@ public class First {
         double e = (a + b + c) / 3;
         System.out.println(d == e);     // true or false?
 
+        // 나지영
+        int a = 20;
+        int b = 21;
+        int c = ++a + b--;
+        int d = --c - 3;
+        System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+        // a = ?, b = ?, c = ?, d = ?
+
+        System.out.println(true + "hello" + false);     // 출력 화면
+        System.out.println(90 + 80 + "hi");             // 출력 화면
+        System.out.println('g' + 'r' + 'a' + 'm');      // 출력 화면
 
     }
 }
